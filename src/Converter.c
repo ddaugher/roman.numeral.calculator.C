949553f82155de2cceee827716a3e4d7e0efec49
DJ Daugherty <ddaugher@gmail.com>
2016-06-22 17:42:25 -0400

    convertToArabic should convert 'II' to 2

--- src/Converter.c
+++ src/Converter.c
@@ -4,6 +4,11 @@
 #include "Converter.h"
 
 const int convertToArabic(const char* character) {
+
+	if ("II" == character) {
+		return 2;
+	}
+	
 	return 1;
 }
 
--- test/ConverterTests.c
+++ test/ConverterTests.c
@@ -4,9 +4,15 @@
 #include "ConverterTests.h"
 #include "../src/Converter.h"
 
-START_TEST(test_first_failing_test)
+START_TEST(test_convert_I_to_1)
    {
     ck_assert_int_eq(1, convertToArabic("I"));
+   }
+END_TEST
+
+START_TEST(test_convert_II_to_2)
+   {
+    ck_assert_int_eq(2, convertToArabic("II"));
    }
 END_TEST
 
@@ -14,7 +20,8 @@ Suite* testSuite(void)
 {
    Suite* suite = suite_create("Converter Tests");
    TCase* romanToArabicTestCase = tcase_create("Convert Roman to Arabic");
-   tcase_add_test(romanToArabicTestCase, test_first_failing_test);
+   tcase_add_test(romanToArabicTestCase, test_convert_I_to_1);
+   tcase_add_test(romanToArabicTestCase, test_convert_II_to_2);
    suite_add_tcase(suite, romanToArabicTestCase);
 
    return suite;
