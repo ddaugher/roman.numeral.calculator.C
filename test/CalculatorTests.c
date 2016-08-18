250a3b796785bf404b227cc72ba126f4255eabf1
DJ Daugherty <ddaugher@gmail.com>
2016-08-17 21:52:33 -0400

    refactor... setting up next set of tests

--- test/CalculatorTests.c
+++ test/CalculatorTests.c
@@ -55,7 +55,7 @@ START_TEST(test_subtract_CLXXI_and_CLXI_equals_IX)
 }
 END_TEST
 
-START_TEST(test_when_first_value_is_null_should_return_error)
+START_TEST(test_when_adding_and_first_value_is_null_should_return_error)
 {
     char buf[MAX_ROMAN_LENGTH];
     add(buf, NULL, "I");
@@ -63,7 +63,7 @@ START_TEST(test_when_first_value_is_null_should_return_error)
 }
 END_TEST
 
-START_TEST(test_when_second_value_is_null_should_return_error)
+START_TEST(test_when_adding_and_second_value_is_null_should_return_error)
 {
     char buf[MAX_ROMAN_LENGTH];
     add(buf, "I", NULL);
@@ -77,8 +77,8 @@ Suite* calculatorTestsSuite(void) {
     tcase_add_test(addRomanTestCase, test_add_I_and_I_equals_II);
     tcase_add_test(addRomanTestCase, test_add_I_and_IV_equals_V);
     tcase_add_test(addRomanTestCase, test_add_X_and_C_equals_CX);
-    tcase_add_test(addRomanTestCase, test_when_first_value_is_null_should_return_error);
-    tcase_add_test(addRomanTestCase, test_when_second_value_is_null_should_return_error);
+    tcase_add_test(addRomanTestCase, test_when_adding_and_first_value_is_null_should_return_error);
+    tcase_add_test(addRomanTestCase, test_when_adding_and_second_value_is_null_should_return_error);
     TCase *subtractRomanTestCase = tcase_create("subtract");
     tcase_add_test(subtractRomanTestCase, test_subtract_X_and_I_equals_IX);
     tcase_add_test(subtractRomanTestCase, test_subtract_CCCXC_and_CLXI_equals_IX);
