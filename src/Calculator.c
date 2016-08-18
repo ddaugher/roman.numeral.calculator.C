2eb2d569f7d734981b043368cab2f3e16f2b7f0f
DJ Daugherty <ddaugher@gmail.com>
2016-08-17 21:54:13 -0400

    handle first value null for subtract

--- src/Calculator.c
+++ src/Calculator.c
@@ -14,5 +14,8 @@ void add(char* result, const char* val1, const char* val2){
 }
 
 void subtract(char* result, const char* val1, const char* val2){
+
+	if (val1 == NULL) { strncpy(result, "ERROR_VAL_NULL", MAX_ROMAN_LENGTH); return; }
+
     convertToRoman(result, convertToArabic(val1) - convertToArabic(val2));
 }
