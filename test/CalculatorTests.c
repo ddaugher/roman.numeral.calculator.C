0d896da633002a30e4b5be2f7fb377fb801bf3c3
DJ Daugherty <ddaugher@gmail.com>
2016-08-17 22:04:18 -0400

    handling add with both input as null

--- test/CalculatorTests.c
+++ test/CalculatorTests.c
@@ -71,6 +71,14 @@ START_TEST(test_when_adding_and_second_value_is_null_should_return_error)
 }
 END_TEST
 
+START_TEST(test_when_adding_and_both_values_are_null_should_return_error)
+{
+    char buf[MAX_ROMAN_LENGTH];
+    add(buf, NULL, NULL);
+    ck_assert_str_eq("ERROR_VAL_NULL", buf);
+}
+END_TEST
+
 START_TEST(test_when_subtracting_and_first_value_is_null_should_return_error)
 {
     char buf[MAX_ROMAN_LENGTH];
@@ -95,6 +103,7 @@ Suite* calculatorTestsSuite(void) {
     tcase_add_test(addRomanTestCase, test_add_X_and_C_equals_CX);
     tcase_add_test(addRomanTestCase, test_when_adding_and_first_value_is_null_should_return_error);
     tcase_add_test(addRomanTestCase, test_when_adding_and_second_value_is_null_should_return_error);
+    tcase_add_test(addRomanTestCase, test_when_adding_and_both_values_are_null_should_return_error);
     TCase *subtractRomanTestCase = tcase_create("subtract");
     tcase_add_test(subtractRomanTestCase, test_subtract_X_and_I_equals_IX);
     tcase_add_test(subtractRomanTestCase, test_subtract_CCCXC_and_CLXI_equals_IX);
