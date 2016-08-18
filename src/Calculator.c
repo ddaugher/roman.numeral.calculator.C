3204f8c0f690e2089704342464949e401d041544
DJ Daugherty <ddaugher@gmail.com>
2016-08-17 22:03:00 -0400

    refactor

--- src/Calculator.c
+++ src/Calculator.c
@@ -6,16 +6,30 @@
 #include "GlobalDefines.h"
 #include "Converter.h"
 
+static bool input_contains_null(const char* val1, const char* val2) {
+
+	if (val1 == NULL || val2 == NULL) return true;
+
+	return false;
+
+}
+
 void add(char* result, const char* val1, const char* val2){
 
-	if (val1 == NULL || val2 == NULL) { strncpy(result, "ERROR_VAL_NULL", MAX_ROMAN_LENGTH); return; }
+	if (input_contains_null(val1, val2)) {
+		strncpy(result, "ERROR_VAL_NULL", MAX_ROMAN_LENGTH); 
+		return;
+	}
 
     convertToRoman(result, convertToArabic(val1) + convertToArabic(val2));
 }
 
 void subtract(char* result, const char* val1, const char* val2){
 
-	if (val1 == NULL || val2 == NULL) { strncpy(result, "ERROR_VAL_NULL", MAX_ROMAN_LENGTH); return; }
+	if (input_contains_null(val1, val2)) {
+		strncpy(result, "ERROR_VAL_NULL", MAX_ROMAN_LENGTH); 
+		return;
+	}
 
     convertToRoman(result, convertToArabic(val1) - convertToArabic(val2));
 }
